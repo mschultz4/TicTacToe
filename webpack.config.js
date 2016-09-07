0f54f35f20b833381787918c82e4ec2e0c0e7164
Matthew Schultz <mschultz4@gmail.com>
2016-09-07 00:39:05 +0000

    added webpack config

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+var path = require("path");
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: __dirname + '/src/index.html',
+  filename: 'index.html',
+  inject: 'body'
+});
+
+module.exports = {
+    entry: "./src/app.js",
+    output: {
+        path: path.join(__dirname, "dist"),
+        filename: "bundle.js"
+    },
+    externals: { 
+        react: "React",
+        "react-dom": "ReactDOM"
+    } ,
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: ["babel"],
+            query: {
+        presets: ['es2015', 'react']
+      }
+      
+        }]
+    },
+    plugins: [HTMLWebpackPluginConfig]
+};
